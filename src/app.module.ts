ae8fe44d6d9841f419041d5da6b30ff61f9b3ae0
Sebastian-Burgos19 <sebassasa10@gmail.com>
2025-04-05 14:29:31 -0500

    actualización

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
+import { CandidatosModule } from './candidatos/candidatos.module';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [UsuarioModule, CandidatosModule],
   controllers: [AppController],
   providers: [AppService],
 })
